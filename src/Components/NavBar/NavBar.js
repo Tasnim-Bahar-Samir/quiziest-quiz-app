fb1108d676ad15862480e4f03b5e9628d22ebace
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-10-11 00:30:26 +0600

    added navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import './NavBar.css'
 import { NavLink } from 'react-router-dom'
+import logo from '../../Images/logo.png'
 
 const NavBar = () => {
   return (
-    <div>
-        <div>
-            
+    <div className='nav-container'>
+        <div className='logo'>
+            <img src={logo} alt="" />
+            <h2>Quiziest</h2>
         </div>
-        <div>
+        <div className='nav-link'>
             <NavLink to='/'>Topics</NavLink>
             <NavLink to='/statistics'>Statistics</NavLink>
             <NavLink to='/blog'>Blog</NavLink>
